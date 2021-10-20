f3e4df355232750a4911959b33ee8e7143bc0f85
Trevor-Warthman <t.dubs371@gmail.com>
2021-10-20 17:39:56 -0400

    initial-setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+	<div id="appDiv">
+		<div id="blog-header">
+			<img alt="Trevor Logo" src="./assets/logo.png" id="corner-logo"/>
+			<p>Welcome to my blog page, where I keep track of some useful web development tricks and best practices as I learn.</p>
+		</div>
+		<div id="post-container">
+			<Post v-for="post in this.shownPosts" :key="post.postNumber" :postInfo="post"></Post>
+		</div>
+		<button @click="showMorePosts()"></button>
+	</div>
+</template>
+
+<script>
+  //papee white, dark grey, orange, and cyan. 
+import Post from "./components/Post.vue";
+export default {
+	name: "App",
+	components: {
+	Post,
+	},
+  data() {
+		return {
+			allPosts: [],
+			postsPerPage: 30,
+			unshownPosts: [],
+			shownPosts: [],
+		} 
+	},
+  created() {
+		//perform call to get all post ids, put in here, then we can put the first 30 in the thing
+		this.allPosts.push({
+			id: 1, 
+			postNumber: 1,
+			title: "Test 1", 
+			body: "today i learned some crap."
+		});
+		this.allPosts.push({
+			id: 2, 
+			postNumber: 2,
+			title: "Test 2", 
+			body: "Cold fusion is bad."
+		});
+
+	this.divideUpPosts();
+	
+	
+  },
+  methods: {
+		showMorePosts() {
+			if(this.unshownPosts.length > 0){
+				this.shownPosts.push(this.unshownPosts.splice(0,this.postsPerPage));
+			}
+		},
+		divideUpPosts(){
+			this.shownPosts = this.shownPosts.concat(this.allPosts.slice(0,30));
+			this.unshownPosts = this.unshownPosts.concat(this.allPosts.slice(this.postsPerPage,this.allPosts.length));
+		},
+	},
+};
+</script>
+
+<style>
+	body, div#appDiv, div#post-container {
+		background-color: #221D23;
+	}
+	body div {
+		background-color: #F9F9F3;
+	}
+
+
+</style>
+<style scoped>
+	#corner-logo {
+		left: 20px;
+		top: 20px;
+
+	}
+
+	#blog-header {
+		margin-bottom: 3vh;
+		display: flex;
+		justify-content: center;
+		height: 10vh;
+	}
+
+	#post-container {
+	
+	}
+</style>
---
+++ frontend/src/components/Post.vue
@@ -0,0 +1,60 @@
+<template>
+	<div :id="'post-' + postInfo.id" class="post">	
+		<div class="post-head">
+			{{this.postInfo.title}}
+		</div>
+		<div class="post-body">
+			{{this.postInfo.body}}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Post',
+	props: {	
+		postInfo: {
+			type: Object,
+			required: true
+		}
+	},
+	created(){
+		console.log(this.postInfo.title)
+	},
+	mounted(){
+		console.log(this.postInfo)
+	}
+}
+</script>
+
+<style scoped>
+/*
+FAEDCA
+7B435B
+3A606E
+CC5803
+2DC2BD 
+
+or
+
+F2F7F2
+39A9DB - blue
+221D23 - dark
+FA8334 - orange
+*/
+
+.post {
+	width: 80vw;
+	margin-left: auto;
+	margin-right: auto;
+	margin-top: 3vh;
+	background-color: #F2F7F2;
+	border: 2px solid #F2F7F2;
+}
+
+.post-head {
+	color: #FA8334;
+	font-weight: bold;
+	
+}
+</style>
