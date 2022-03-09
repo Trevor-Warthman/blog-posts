3e6f5dd3653d7502f49bd3289c8be6e388312ee0
Trevor-Warthman <t.dubs371@gmail.com>
2022-03-08 20:23:35 -0500

    Initial set up of firebase and re-init vue-cli

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBWvDsaIFyTRyw7jdl3Cu38bsFtg0m9wjU",
+  authDomain: "blog-posts-54942.firebaseapp.com",
+  projectId: "blog-posts-54942",
+  storageBucket: "blog-posts-54942.appspot.com",
+  messagingSenderId: "1077907260413",
+  appId: "1:1077907260413:web:a888bd89a3417016344f94",
+  measurementId: "G-ZZCK0SRXKX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
 createApp(App).mount('#app')
